d3e5883410f63095b8f26afc3c18a21df36f1d84
Hossam Sherif <husam287@hotmail.com>
2020-11-15 13:24:32 +0200

    Iniziallize doctor profile
    -add profile
    -add card and details in the profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { DoctorCardComponent } from './doctor-profile/doctor-card/doctor-card.component';
+import { DoctorDetailsComponent } from './doctor-profile/doctor-details/doctor-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorProfileComponent,
+    DoctorCardComponent,
+    DoctorDetailsComponent
   ],
   imports: [
     BrowserModule,
