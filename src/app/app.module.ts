ba0458faaaf50b1a0f3fcaec07be9de3843a1ef2
Hazem <hazemali627@gmail.com>
2020-11-17 23:10:30 +0200

    advices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImproveYourBodyComponent } from './improve-your-body/improve-your-body.component';
+import { AdvicesComponent } from './advices/advices.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { DietComponent } from './diet/diet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImproveYourBodyComponent,
+    AdvicesComponent,
+    WorkoutsComponent,
+    DietComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/improve-your-body/improve-your-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-improve-your-body',
+  templateUrl: './improve-your-body.component.html',
+  styleUrls: ['./improve-your-body.component.css']
+})
+export class ImproveYourBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
