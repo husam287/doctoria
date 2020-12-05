3c83c8aa3c86a8f476e8a2e262f08b5bae45099b
hader afat <74070787+hader-afat@users.noreply.github.com>
2020-12-05 16:44:54 +0200

    card with filter v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
