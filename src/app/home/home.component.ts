f0c320f52b90ad3a0746d943cc6cb5a3eb84f093
iamMai <maitarek145@gmail.com>
2020-12-06 13:53:41 +0200

    Navbar ,slider and Footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,8 @@ export class HomeComponent implements OnInit {
   constructor() {  }
 
    ngOnInit(): void {
-      myFunction()
   }
   
 }
-function  myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
+
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,9 +14,6 @@ export class SearchComponent implements OnInit {
   
            
 }
-$('.dropdown-menu li').on('click', function() {
-        var getValue = $(this).text();
-        $('.dropdown-select').text(getValue);
-});
+
  
  
