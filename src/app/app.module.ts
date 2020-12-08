4688aa6766e9b0274d7d6b471f1b0c3b91ec2f1d
Hossam Sherif <husam287@hotmail.com>
2020-12-08 22:20:48 +0200

    finsh photo roll and search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { EmailsComponent } from './emails/emails.component';
-import { SearchComponent } from './search/search.component';
-import { EndComponent } from './end/end.component';
+import { EmailsComponent } from './home/emails/emails.component';
+import { SearchComponent } from './home/search/search.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { EndComponent } from './end/end.component';
     HomeComponent,
     EmailsComponent,
     SearchComponent,
-    EndComponent
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EndComponent } from './end.component';
+import { NavBarComponent } from './nav-bar.component';
 
-describe('EndComponent', () => {
-  let component: EndComponent;
-  let fixture: ComponentFixture<EndComponent>;
+describe('NavBarComponent', () => {
+  let component: NavBarComponent;
+  let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EndComponent ]
+      declarations: [ NavBarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EndComponent);
+    fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
