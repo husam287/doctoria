844215eefc8d8589b4844968ec14f8237f4033f5
Hazem <hazemali627@gmail.com>
2020-12-13 18:40:29 +0200

    first-creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DoctorHomeComponent } from './doctor-home/doctor-home.component';
 import { DoctorDashboardComponent } from './doctor-dashboard/doctor-dashboard.component';
 import { HomeComponent } from './doctor-dashboard/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DoctorHomeComponent,
     DoctorDashboardComponent,
     HomeComponent
   ],
