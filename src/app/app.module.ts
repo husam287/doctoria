315f0cc8e3d5802a117b907b5e02db752917de04
Hazem <hazemali627@gmail.com>
2020-12-14 07:36:45 +0200

    sidebar & statistics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DoctorDashboardComponent } from './doctor-dashboard/doctor-dashboard.component';
 import { HomeComponent } from './doctor-dashboard/home/home.component';
+import { SidebarComponent } from './doctor-dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DoctorDashboardComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
