aba56a4bd6c0404655a2cbd598c98301a1a3589d
hader afat <74070787+hader-afat@users.noreply.github.com>
2020-12-16 20:17:20 +0200

    add appointments and patient card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { FilterComponent } from './filter/filter.component';
 import { DoctorsComponent } from './doctors/doctors.component';
 import { PatientCardComponent } from './patient-profile/patient-card/patient-card.component';
+import { AppointmentsComponent } from './patient-profile/appointments/appointments.component';
+import { HistoryComponent } from './patient-profile/history/history.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +19,9 @@ import { PatientCardComponent } from './patient-profile/patient-card/patient-car
     CardsComponent,
     FilterComponent,
     DoctorsComponent,
-    PatientCardComponent
+    PatientCardComponent,
+    AppointmentsComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
