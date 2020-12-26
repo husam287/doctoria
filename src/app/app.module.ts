53fd389304334efb8a1837e1f4f326868ff2e000
Hossam Sherif <husam287@hotmail.com>
2020-12-27 00:45:25 +0200

    edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -67,7 +68,8 @@ import { PatientCardComponent } from './patient-profile/patient-card/patient-car
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from './User.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,13 +11,32 @@ import { User } from './User.model';
 export class SignUpComponent implements OnInit {
 @ViewChild('f1') signupform:NgForm;
   submitted = false;
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
   onSubmit(){
     this.submitted=true;
     const user:User=this.signupform.value;
+
+    console.log(user);
+    this.http.post("http://localhost:8080/api/users/signup",user).toPromise()
+    .then((result)=>{
+      console.log('succ:',result);
+    })
+    .catch(err=>{
+      console.log('error:',err);
+    })
+
+
+  }
+
+  test(){
+    this.http.get("http://localhost:8080/api/doctors/all").toPromise()
+    .then((doctors)=>{
+      console.log(doctors);
+    })
+    
   }
 
 }
