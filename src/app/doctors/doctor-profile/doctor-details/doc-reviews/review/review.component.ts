5f0350701b7bd79c7884e4b5b98a8bd45d273029
Hazem <hazemali627@gmail.com>
2020-12-27 08:11:35 +0200

    nothing

--- src/app/doctors/doctor-profile/doctor-details/doc-reviews/review/review.component.ts
+++ src/app/doctors/doctor-profile/doctor-details/doc-reviews/review/review.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -13,22 +13,24 @@ import { Review } from '../../../../../models/Review.model';
 })
 export class ReviewComponent implements OnInit {
 
-  reviews:[Review]
-  constructor(private route:Router,private http: HttpClient, private route1: ActivatedRoute) { this.route1.params
-  .subscribe(params => console.log(params) );}
+  reviews: [Review]
+  constructor(private route: Router, private http: HttpClient, private route1: ActivatedRoute) {
+    this.route1.params
+    .subscribe(params => console.log(params));
+  }
 
   ngOnInit(): void {
 
     const id = this.route1.parent.snapshot.params['id'];
-   this.http.get<Doctor>('http://localhost:8080/api/doctors/'+id).toPromise()
-   .then((doctor) =>{
-     console.log('succ:',doctor);
-      this.reviews = doctor.reviews;
-   })
-   .catch(err =>{
-     console.log('error:',err);
+    this.http.get<Doctor>('http://localhost:8080/api/doctors/' + id).toPromise()
+      .then((doctor) => {
+        console.log('succ:', doctor);
+        this.reviews = doctor.reviews;
+      })
+      .catch(err => {
+        console.log('error:', err);
 
- })
+      })
 
   }
 
--- src/app/patient-profile/patient-card/patient-card.component.ts
+++ src/app/patient-profile/patient-card/patient-card.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-patient-card',
-  templateUrl: './patient-card.component.html',
-  styleUrls: ['./patient-card.component.css']
+	selector: 'app-patient-card',
+	templateUrl: './patient-card.component.html',
+	styleUrls: ['./patient-card.component.css']
 })
 export class PatientCardComponent implements OnInit {
 
-  constructor() { }
+	constructor(private route: Router, private http: HttpClient, private route1: ActivatedRoute) {
 
-  ngOnInit(): void {
-  }
+		this.route1.params
+			.subscribe(params => console.log(params));
 
- 
+
+	}
+
+	ngOnInit(): void {
+		
+
+
+
+
+	}
+
+
 }
