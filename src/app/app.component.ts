72185a383c8baefc2c21dbc72e97b16885bdc687
Hossam Sherif <husam287@hotmail.com>
2020-12-28 00:37:28 +0200

    fix login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignUpService } from './sign-up/sign-up.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
  
   title = 'doctoria';
+
+  constructor(private signupService:SignUpService){}
+
+  ngOnInit(){
+    this.signupService.autoLogIn();
+  }
+
 }
