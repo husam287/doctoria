d09a8a22e1a2df58f511f12a5b099f8bc25976cc
Hazem <hazemali627@gmail.com>
2020-12-27 09:39:34 +0200

    encapsulating patient components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {​​ Routes , RouterModule }​​ from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,44 +10,46 @@ import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PatientProfileComponent } from './patient-profile/patient-profile.component';
 import { SidebaarComponent } from './patient-profile/sidebaar/sidebaar.component';
-import { EditBasicComponent } from './edit-basic/edit-basic.component';
-import { EditPatientComponent } from './edit-patient/edit-patient.component';
-import { HistoryComponent } from './history/history.component';
-import { AppointmentsComponent } from './appointments/appointments.component';
+import { EditBasicComponent } from './patient-profile/edit-basic/edit-basic.component';
+import { EditPatientComponent } from './patient-profile/edit-patient/edit-patient.component';
+import { HistoryComponent } from './patient-profile/history/history.component';
+import { AppointmentsComponent } from './patient-profile/appointments/appointments.component';
 const appRoutes: Routes = [
-     //{path:'',component:SidebaarComponent},
-    //{path:'patient-profile',component:PatientProfileComponent},
-     
-     { path :'appointments',component:AppointmentsComponent},
-     { path:'history',component:HistoryComponent},
-     {path:'edit-basic',component:EditBasicComponent},
-     {path:'edit-patient',component:EditPatientComponent},
-     {path:'home',component:HomeComponent},
-     
-    
+	//{path:'',component:SidebaarComponent},
+	{path:'patient-profile',component:PatientProfileComponent, children: [
+		{ path: 'appointments', component: AppointmentsComponent },
+		{ path: 'history', component: HistoryComponent },
+		{ path: 'edit-basic', component: EditBasicComponent },
+		{ path: 'edit-patient', component: EditPatientComponent },
+		{ path: 'home', component: HomeComponent }
+
+	]},
+
+
+
 ];
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    EmailsComponent,
-    SearchComponent,
-    FooterComponent,
-    NavBarComponent,
-    PatientProfileComponent,
-    SidebaarComponent,
-    EditBasicComponent,
-    EditPatientComponent,
-    HistoryComponent,
-    AppointmentsComponent
-    
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		EmailsComponent,
+		SearchComponent,
+		FooterComponent,
+		NavBarComponent,
+		PatientProfileComponent,
+		SidebaarComponent,
+		EditBasicComponent,
+		EditPatientComponent,
+		HistoryComponent,
+		AppointmentsComponent
+
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		RouterModule.forRoot(appRoutes)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
